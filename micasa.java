807443f8abbe896fd59bdb529aaf5759ad759827
Naissance <naissance37@gmail.com>
2019-12-17 03:41:11 +0200

    dd

--- micasa.java
+++ micasa.java
@@ -1,8 +1,13 @@
+import person.class;
+
 class nuapp{
 
 public static  void main(String[] ars){
 
     System.out.println("Hi people");
+    person p=new person("ACHILLES",33);
+
+    System.out.println("RECORD :"+ p.toString());
 }
 
 }
